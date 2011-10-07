71386392b257404568430b2521c30c0612bdea5a
Christof Musik <musik@bitbone.de>
2011-10-07 12:06:17 +0200

    added ability to translate module titles

---
+++ tools/update-module-title.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ ! -f module.info ]; then
+	echo "File module.info not found."
+	exit 0
+fi
+
+for l in "de" "nl" "fr"; do
+	title=$(awk -F= '/^moduletitle/ { print "desc_" "'$l'" "=" $2 }' lang/$l)
+
+	if [ -z $title ]; then
+		title=$(awk -F= '/^moduletitle/ { print "desc_" "'$l'" "=" $2 }' lang/en)
+	fi
+
+
+	if grep -q "desc_$l=" module.info; then
+		sed -e "s/desc_$l=.*/$title/" -i module.info
+	else
+		echo $title >> module.info
+	fi
+done
