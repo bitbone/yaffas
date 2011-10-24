9b867ee480f9b7eff3fbab2e2a141f87e2d20b52
Christof Musik <musik@bitbone.de>
2011-10-24 16:07:06 +0200

    fixed script for brazil lang support; fixed problem with script

--- tools/update-module-title.sh
+++ tools/update-module-title.sh
@@ -5,10 +5,10 @@ if [ ! -f module.info ]; then
 	exit 0
 fi
 
-for l in "de" "nl" "fr"; do
+for l in "de" "nl" "fr" "pt_BR"; do
 	title=$(awk -F= '/^moduletitle/ { print "desc_" "'$l'" "=" $2 }' lang/$l)
 
-	if [ -z $title ]; then
+	if [ -z "$title" ]; then
 		title=$(awk -F= '/^moduletitle/ { print "desc_" "'$l'" "=" $2 }' lang/en)
 	fi
 
