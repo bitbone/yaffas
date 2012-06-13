ad452d8f407b63db45e3cb77aa6107ae24ed3017
Christof Musik <musik@bitbone.de>
2012-06-13 08:21:59 +0200

    fixed reopened ADM-61

--- zarafa/webmin/modules/yaffas-module-zarafaresources/lib/ZarafaResources.pm
+++ zarafa/webmin/modules/yaffas-module-zarafaresources/lib/ZarafaResources.pm
@@ -84,6 +84,10 @@ sub create_resource ($$$$$$) {
 		}
 	}
 
+	if ($email =~ /.*\@$/) {
+		throw Yaffas::Exception("err_no_domains");
+	}
+
 	Yaffas::UGM::add_user( $resource, undef, 'resource', $description );
 	Yaffas::UGM::set_email( $resource, $email ); # set email seperate to avoid warning about incorrect email address
 	modify_resource( $resource, $description, $decline_conflict, $decline_recurring, $type, $capacity );
