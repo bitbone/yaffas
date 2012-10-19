416a8d4d3b934bd5849a712c6d8c5ceb30fe68d9
André Welter <welter@bitbone.de>
2012-10-19 11:32:03 +0200

    * execute upgrade scripts from yaffas-config postinst

---
+++ base/yaffas-config/yaffas-upgrade.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LOGFILE=/root/yaffas-upgrade.log
+if [ -e $LOGFILE ]; then
+	mv $LOGFILE ${LOGFILE}.old
+fi
+
+echo "Starting upgrade procedures ..." >> $LOGFILE
+
+for UPGRADE in /opt/yaffas/share/yaffas-upgrade/*.sh; do
+	echo $(basename $UPGRADE) >> $LOGFILE
+	sh $UPGRADE
+done
+
+echo "... done" >> $LOGFILE
