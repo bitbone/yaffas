bitbone/yaffas

commit a79c0f0321380483a23a3054fdeb82b1379052b4

SNMP config improvements as per ADM-226, ADM-224
This commit introduces some important changes to the SNMP config
module.
We previously tried to patch the existing snmpd.confs, which is no
longer feasable because of distribution-specific changes.

We now replace the existing config with a yaffas-provided, standardized
file and adjust this one as needed. The old config is moved to
.yaffassave.

This fixes
  - ADM-226 (SNMP is broken on Ubuntu 12.04 and Debian 6)
  - SNMP configuration produces totally different behaviour depending on the distribution

base/webmin/modules/yaffas-module-snmpconf/lib/SNMPConf.pm
@@ -13,6 +13,32 @@ use Error qw(:try);
 
 our @ISA = qw(Yaffas::Module);
 
+our $DEFAULT_CONFIG = <<'EOT';
+# yaffas-1.3 configuration - DO NOT MOVE OR DELETE THIS LINE
+
+# COMMUNITYs
+#       sec.name source    community
+com2sec readonly 127.0.0.1 public
+
+# GROUPs
+#             	sec.model  sec.name
+group MyROGroup v1         readonly
+group MyROGroup v2c        readonly
+group MyROGroup usm        readonly
+
+# VIEWs
+#           incl/excl subtree                          mask
+view all    included  .1                               80
+
+# ACLs
+#                context sec.model sec.level match  read   write  notif
+access MyROGroup ""      any       noauth    exact  all    none   none
+
+# System information
+syslocation Unknown (configure /etc/snmp/snmpd.local.conf)
+syscontact Root <root@localhost> (configure /etc/snmp/snmpd.local.conf)
+EOT
+
 =head1 NAME
 
 Yaffas::Module::SNMPConf - Functions for SNMP configuration
@@ -45,16 +71,11 @@ sub set_snmp_config($;$$) {
 			$community = "public";
 		}
 
-		if (defined($network) and $network ne "default") {
-			my $check = 0;
-			if ($network =~ m/(.*)\/(.*)$/) {
-				$check = Yaffas::Check::ip($1, $2, "netaddr");
-			} else {
-				$check = Yaffas::Check::ip($network);
-			}
-			throw Yaffas::Exception("err_network_string") unless ($check)
-		} else {
-			$network = "default";
+		$network = "127.0.0.1" unless defined $network;
+		if ($network ne "default" &&
+				!Yaffas::Check::ip($1, $2, "netaddr") &&
+				!Yaffas::Check::ip($network)) {
+			throw Yaffas::Exception("err_network_string");
 		}
 
 		if(Yaffas::Constant::OS =~ m/RHEL\d/ ) {
@@ -70,14 +91,26 @@ sub set_snmp_config($;$$) {
 
 		throw Yaffas::Exception("err_file_read", Yaffas::Constant::FILE->{snmpd_conf}) unless($file->get_content());
 
+		if (!config_has_yaffas_tag($file)) {
+			# do a backup:
+			my $bkpfile = Yaffas::File->new(
+				Yaffas::Constant::FILE->{snmpd_conf} . '.yaffassave');
+			my @oldlines = $file->get_content();
+			$bkpfile->set_content(\@oldlines);
+			$bkpfile->save() or
+				throw Yaffas::Exception("err_file_write",
+					Yaffas::Constant::FILE->{snmpd_conf} . '.yaffassave');
+
+			# we have permission to replace the file with our default
+			# config, so lets do that
+			my @lines = split(/\n/, $DEFAULT_CONFIG);
+			$file->set_content(\@lines);
+		}
+
 		my @lines = $file->search_line(qr/^com2sec/);
 
 		foreach (@lines) {
-		if(Yaffas::Constant::OS =~ m/RHEL\d/ ) {
-			$file->splice_line($_, 1, "com2sec  notConfigUser $network $community");
-		} else {
 			$file->splice_line($_, 1, "com2sec readonly $network $community");
-		}
 		}
 		$file->save() or throw Yaffas::Exception("err_file_write", Yaffas::Constant::FILE->{snmpd_conf});
 
@@ -101,6 +134,27 @@ sub set_snmp_config($;$$) {
 			system(Yaffas::Constant::APPLICATION->{"update-rc.d"}, "-f", "snmpd", "remove");
 		}
 	}
+}
+
+=over
+
+=item config_has_yaffas_tag(FILEOBJ)
+
+FILEOBJ - Yaffas::File instance
+
+Returns whether the given config file contains the yaffas tag.
+
+=cut
+
+sub config_has_yaffas_tag(;$) {
+	my $file = shift;
+	if (!defined($file)) {
+		$file = Yaffas::File->new(Yaffas::Constant::FILE->{snmpd_conf});
+		throw Yaffas::Exception("err_file_read",
+			Yaffas::Constant::FILE->{snmpd_conf})
+			unless($file->get_content());
+	}
+	return $file->get_content(0) =~ qr/^\s*#\s*yaffas-\S+ configuration/;
 }
 
 sub _set_snmp_config($;$$) {
@@ -138,6 +192,15 @@ sub get_snmp_config() {
 			last;
 		}
 	}
+	if (!config_has_yaffas_tag($file)) {
+		if (!$vals[3]) {
+			$vals[3] = "public";
+		}
+		if (!$vals[2] || $vals[2] eq "default") {
+			$vals[2] = "127.0.0.1";
+		}
+	}
+
 	my $enabled;
 	if(Yaffas::Constant::OS =~ m/RHEL\d/ ) {
 		$enabled = -l "/etc/rc2.d/S50snmpd" ? 1 : 0;

base/webmin/modules/yaffas-module-snmpconf/snmpconf/check_yaffas_tag.cgi
@@ -1,0 +1,42 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Yaffas;
+use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
+use Yaffas::Exception;
+use Error qw(:try);
+use JSON;
+use Yaffas::Module::SNMPConf;
+
+Yaffas::init_webmin();
+ReadParse();
+Yaffas::json_header();
+
+try {
+	print to_json({
+		'has_tag' => Yaffas::Module::SNMPConf::config_has_yaffas_tag()
+	});
+} catch Yaffas::Exception with {
+	print Yaffas::UI::all_error_box(shift);
+};
+
+=pod
+
+=head1 COPYRIGHT
+
+This file is part of yaffas.
+
+yaffas is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+yaffas is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+License for more details.
+
+You should have received a copy of the GNU Affero General Public
+License along with yaffas.  If not, see
+<http://www.gnu.org/licenses/>.

base/webmin/modules/yaffas-module-snmpconf/snmpconf/index.js
@@ -1,0 +1,32 @@
+function SNMPConf(){
+}
+
+SNMPConf.prototype.confirmation = function(url, args, submit) {
+	var callback = {
+		success: function(r) {
+			var result = YAHOO.lang.JSON.parse(r.responseText);
+
+			if (result.has_tag != 1) {
+				// config file will be replaced, so ask for confirmation
+				var d = new Yaffas.Confirm(
+					_("lbl_confirm_replace_config_header"),
+					_("lbl_confirm_replace_config_msg"),
+					submit);
+				d.show();
+			} else {
+				// no users will be deleted, so we proceed without asking
+				submit();
+			}
+		},
+		failure: Yaffas.ui.handleFailure,
+		scope: this
+	};
+
+	YAHOO.util.Connect.asyncRequest("POST", "/snmpconf/check_yaffas_tag.cgi",
+		callback, args);
+
+	// delay submitting
+	return true;
+}
+
+module = new SNMPConf();

base/webmin/modules/yaffas-module-snmpconf/snmpconf/lang/en
@@ -2,6 +2,8 @@ lbl_network=Access for
 lbl_community=Community (Password)
 lbl_enable=Enable SNMP
 lbl_snmp_config=SNMP Configuration
+lbl_confirm_replace_config_header=Replace SNMP configuration?
+lbl_confirm_replace_config_msg=In order for yaffas to be able to perform the requested changes, it has to <strong>overwrite</strong> the current SNMP configuration.<br />Should the config be overwritten?
 
 err_network_string=The access can only be set for IP-Addresses or networks.
 err_community_string=The community-string can only contain digits (0-9) and chars (a-z, A-Z).

commit e2d454b22c643e6451ed0c6df62761a38f37530e

Fixing regression in snmpconf: Using a netmask in "Access for" always lead to an error Related to ADM-224
This was introduced in a79c0f0321380483a23a3054fdeb82b1379052b4.
We used $1 and $2 in a call to Check::ip(), but the regexp which
should have populated $1 and $2 got eaten by the above mentioned
commit.

base/webmin/modules/yaffas-module-snmpconf/lib/SNMPConf.pm
@@ -72,9 +72,9 @@ sub set_snmp_config($;$$) {
 		}
 
 		$network = "127.0.0.1" unless defined $network;
-		if ($network ne "default" &&
-				!Yaffas::Check::ip($1, $2, "netaddr") &&
-				!Yaffas::Check::ip($network)) {
+		if ($network ne "default" && !Yaffas::Check::ip($network) &&
+				($network !~ m/^([0-9\.]+)\/([0-9\.]+)$/ ||
+				!Yaffas::Check::ip($1, $2, "netaddr"))) {
 			throw Yaffas::Exception("err_network_string");
 		}
 