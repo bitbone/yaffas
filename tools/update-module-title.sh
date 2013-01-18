d963964f8a5e58d6de4ed2498d93e147d56ac181
Christian Hoffmann <hoffmann@bitbone.de>
2013-01-18 10:16:15 +0100

    Temporarily revert "Fixing ADM-271 (Chinese translation (zh_TW))"
    This reverts commit 76c036e5b3b46fa0e3ba0749420df43dc5985a8e.

--- tools/update-module-title.sh
+++ tools/update-module-title.sh
@@ -5,7 +5,7 @@ if [ ! -f module.info ]; then
 	exit 0
 fi
 
-for l in "de" "nl" "fr" "pt_BR" "zh_TW"; do
+for l in "de" "nl" "fr" "pt_BR"; do
 	title=$(awk -F= '/^moduletitle/ { print "desc_" "'$l'" "=" $2 }' lang/$l)
 
 	if [ -z "$title" ]; then
