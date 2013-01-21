21b3f327a9a50bffcc519a1d3e86c6d4da8ac4aa
Christian Hoffmann <hoffmann@bitbone.de>
2013-01-21 11:50:58 +0100

    Fixing ADM-289 (Network > Configuration cannot cope with quotes in CentOS ifcfg-* files)
    Regular expressions have been updated to ignore surrounding quotes

--- base/webmin/modules/yaffas-module-netconf/lib/Netconf.pm
+++ base/webmin/modules/yaffas-module-netconf/lib/Netconf.pm
@@ -440,11 +440,11 @@ sub _load_settings {
 
 				foreach my $line ($bkf->get_content()){
 					chomp $line;
-					$ip = $1 if $line =~ m#^IPADDR=(.+)\z#ix;
-					$netmask = $1 if $line =~ m#^NETMASK=(.+)\z#ix;
-					$method = $1 if $line =~ m#^BOOTPROTO=(.+)\z#ix;
+					$ip = $1 if $line =~ m#^IPADDR="?(.+)"?\z#ix;
+					$netmask = $1 if $line =~ m#^NETMASK="?(.+)"?\z#ix;
+					$method = $1 if $line =~ m#^BOOTPROTO="?(.+)"?\z#ix;
 					$method = "static" unless(lc $method eq 'dhcp');
-					push @$dns, $1 if $line =~ m#^DNS\d+=(.+)\z#ix;
+					push @$dns, $1 if $line =~ m#^DNS\d+="?(.+)"?\z#ix;
 				}
 			}
 
@@ -469,7 +469,7 @@ sub _load_settings {
 			}
 
 			my $yf = Yaffas::File->new($fn) || throw Yaffas::Exception("err_file_read", $fn);
-			$settings{$device}->{'ENABLED'} = (defined $yf->search_line("ONBOOT=yes")) ? "onboot" : undef;
+			$settings{$device}->{'ENABLED'} = (defined $yf->search_line('ONBOOT="?yes"?')) ? "onboot" : undef;
 
 		}
 	}
