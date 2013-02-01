7ef55f1717ca9dfcaa4c06a4d724e3d3157391eb
Christian Hoffmann <hoffmann@bitbone.de>
2013-02-01 08:13:00 +0100

    Improving versionbump.sh error handling / usage info

--- tools/versionbump.sh
+++ tools/versionbump.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+error() {
+		echo "$@"
+		exit 1
+}
+
+have_uncommitted_changes() {
+		[[ -n $(git status -s) ]]
+}
+
 VERSION=$1
-BASEDIR="$(dirname $0)"
+[[ $VERSION ]] || error "Syntax: $(basename "$0") 1.2.3-4"
+have_uncommitted_changes && \
+		error "Please commit/stash all your local changes first"
+BASEDIR="$(dirname "$0")"
 find -path '*/debian/changelog' | \
 		grep -vP 'ckeditor|z-push' | \
 		xargs -I '{}' "$BASEDIR/"updatechangelog.sh {} -v $VERSION "update to version $VERSION"
