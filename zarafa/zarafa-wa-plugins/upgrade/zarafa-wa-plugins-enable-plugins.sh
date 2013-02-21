8c7c7ca13d61cacdc71bc5c9a284f1711b7ad604
Christian Hoffmann <hoffmann@bitbone.de>
2013-02-21 16:47:34 +0100

    Proper fix for ADM-304 (WebApp password change plugin not enabled when upgrading from pre-1.3)
    The previous fix did not work. We tried to use perl as the interpreter in the
    shebang line, which cannot work as the upgrade scripts are called with
    "bash /path/to/script". So this is now a bash script with a single call to
    perl.
    Also, a perl module name was wrong.

--- zarafa/zarafa-wa-plugins/upgrade/zarafa-wa-plugins-enable-plugins.sh
+++ zarafa/zarafa-wa-plugins/upgrade/zarafa-wa-plugins-enable-plugins.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env perl -I /opt/yaffas/lib/perl5
-use Yaffas;
-use Yaffas::AuthSrv;
+#!/bin/bash
+# It would be better to have perl as the interpreter, but the framework
+# enforces bash...
 
 # ADM-304
 # the password change plugin had only be enabled when re-setting
 # the authentication server, but it should be enabled on all installs
 # by default now
 # the actual config was/will be written by the AuthSrv module
-Yaffas::AuthSrv::_link_webaccess_plugin("passwd");
-Yaffas::AuthSrv::_link_webapp_plugin("passwd");
+perl -I /opt/yaffas/lib/perl5 \
+		-MYaffas -MYaffas::Module::AuthSrv \
+		-e 'Yaffas::Module::AuthSrv::_link_webaccess_plugin("passwd");Yaffas::Module::AuthSrv::_link_webapp_plugin("passwd");'
