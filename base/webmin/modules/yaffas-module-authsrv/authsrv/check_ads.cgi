bitbone/yaffas

commit 617b543ff24e82ecf159d3cfb609458182bc02b8

Unifying webmin restart calls (fixing ADM-268)
ADM-268: Config restore is executed multiple times and might lead
to an unpredictable system state

These changes drop all the fork()ing code used in different places to
execute webmin restarts. Instead, we handle this special case
directly in Yaffas::Service::control(WEBMIN, RESTART), so that the
caller does not have to care about it.

Instead of forking, we opt for an easier and less problematic method:
we simply ignore SIGTERMs for the currently active connection which
starts the webmin restart init script call. Therefore it is protected
from being killed and the control flow will continue as expected
afterwards.

No more stray children or code running in parallel although it
shouldn't.

base/webmin/modules/yaffas-module-authsrv/authsrv/check_ads.cgi
@@ -83,23 +83,14 @@ try {
 			control(YAFFAS_MAILDISCLAIMERS(), RESTART());
 		}
 
-	# fork, because we have to restart webmin
-	my $pid = fork;
-	if ($pid == 0) {
-		# child
-		try {
-			Yaffas::Service::control(WEBMIN, RESTART);
-		} catch Yaffas::Exception with {
-			print Yaffas::UI::all_error_box(shift);
-		};
-	} else {
-		# parent
-		wait;
-	}
+	try {
+		Yaffas::Service::control(WEBMIN, RESTART);
+	} catch Yaffas::Exception with {
+		print Yaffas::UI::all_error_box(shift);
+	};
 
-		sleep(3);
-		print Yaffas::UI::ok_box();
-	}
+	sleep(3);
+	print Yaffas::UI::ok_box();
 }
 catch Yaffas::Exception with {
 	Yaffas::Service::control(NSCD, START) unless Yaffas::Constant::OS =~ m/RHEL\d/ ;

base/perlmod/yaffas-lib/lib/Yaffas/Service.pm
@@ -936,6 +936,21 @@ sub _stop($$) {
 
 sub _restart($$) {
 	my $tmp;
+	if ($_[0] eq $Yaffas::Service::SERVICES{ WEBMIN() }) {
+		# if webmin has to be restarted, it would kill also this process,
+		# which would reset the connection to the user's browser;
+		# therefore we install a signal handler to IGNORE SIGTERM so that
+		# we can continue serving this request; the process will exit
+		# regularly after the request has been finished;
+		# NOTE: this is supposed to replace several fork-then-system()
+		#       calls which were supposed to do the same; however, this
+		#       did not work properly, so better just use
+		#       control(WEBMIN, RESTART), which ends up here
+		$SIG{'TERM'} = 'IGNORE';
+
+		# continue... the actual restart is handled by the block of
+		# common cases below
+	}
 	if($_[0] eq $Yaffas::Service::SERVICES{ HYLAFAX() } ||
 	   $_[0] eq $Yaffas::Service::SERVICES{ ZARAFA_SERVER() } ||
 	   $_[0] eq $Yaffas::Service::SERVICES{ ZARAFA_MONITOR() } ||

base/webmin/modules/yaffas-module-authsrv/authsrv/check_ldap.cgi
@@ -93,19 +93,11 @@ try {
 				# need to re-read LDAP config
 				control(YAFFAS_MAILDISCLAIMERS(), RESTART());
 			}
-			# fork, because we have to restart webmin
-			my $pid = fork;
-			if ($pid == 0) {
-				# child
-				try {
-					Yaffas::Service::control(WEBMIN, RESTART);
-				} catch Yaffas::Exception with {
-					print Yaffas::UI::all_error_box(shift);
-				};
-			} else {
-				# parent
-				wait;
-			}
+			try {
+				Yaffas::Service::control(WEBMIN, RESTART);
+			} catch Yaffas::Exception with {
+				print Yaffas::UI::all_error_box(shift);
+			};
 
 			print Yaffas::UI::ok_box();
 		} else {

base/webmin/modules/yaffas-module-authsrv/authsrv/check_local_ldap.cgi
@@ -78,19 +78,11 @@ try
 	}
 	Yaffas::File->new(Yaffas::Constant::FILE->{auth_wizard_lock}, 1)->save();
 
-	# fork, because we have to restart webmin
-	my $pid = fork;
-	if ($pid == 0) {
-		# child
-		try {
-			Yaffas::Service::control(WEBMIN, RESTART);
-		} catch Yaffas::Exception with {
-			print Yaffas::UI::all_error_box(shift);
-		};
-	} else {
-		# parent
-		wait;
-	}
+	try {
+		Yaffas::Service::control(WEBMIN, RESTART);
+	} catch Yaffas::Exception with {
+		print Yaffas::UI::all_error_box(shift);
+	};
 
 	print Yaffas::UI::ok_box();
 }

base/webmin/modules/yaffas-module-authsrv/authsrv/check_pdc.cgi
@@ -69,19 +69,11 @@ try {
 		control(YAFFAS_MAILDISCLAIMERS(), RESTART());
 	}
 
-	# fork, because we have to restart webmin
-	my $pid = fork;
-	if ($pid == 0) {
-		# child
-		try {
-			Yaffas::Service::control(WEBMIN, RESTART);
-		} catch Yaffas::Exception with {
-			print Yaffas::UI::all_error_box(shift);
-		};
-	} else {
-		# parent
-		wait;
-	}
+	try {
+		Yaffas::Service::control(WEBMIN, RESTART);
+	} catch Yaffas::Exception with {
+		print Yaffas::UI::all_error_box(shift);
+	};
 
 	print Yaffas::UI::ok_box();
 } 

base/webmin/modules/yaffas-module-certificate/lib/Certificate.pm
@@ -356,21 +356,7 @@ sub restart_service($) {
 
 	if (@restart) {
 		foreach my $service (@restart) {
-			my $pid = fork();
-			throw Yaffas::Exception('err_fork') unless defined $pid;
-
-			if ($pid) {
-				# parent
-				wait;
-				exit 0;
-				# wird webmin neu gestartet. so wird das hier gekillt, vom
-				# kind. wird ein andere dienst gerestartet, so wird der
-				# vater nach dem ende des kindes weiterlaufen, dies wird
-				# mit exit 0 verindert. ( oder? )
-			}else {
-				# child
-				Yaffas::Service::control($services{$service}, Yaffas::Service::RESTART());
-			}
+			Yaffas::Service::control($services{$service}, Yaffas::Service::RESTART());
 		}
 		return 1;
 	}

base/webmin/modules/yaffas-module-changepw/changepw/check_adminpass.cgi
@@ -21,26 +21,16 @@ my $pass1 = $main::in{'pass1'};
 my $pass2 = $main::in{'pass2'};
 
 
-# fork, because we have to restart webmin
-my $pid = fork;
-if ($pid == 0) {
-	# child
-	try {
-		Yaffas::Module::ChangePW::check_passwords($pass1, $pass2);
-		Yaffas::Module::ChangePW::change_admin_password($pass1);
+try {
+	Yaffas::Module::ChangePW::check_passwords($pass1, $pass2);
+	Yaffas::Module::ChangePW::change_admin_password($pass1);
 
-		print Yaffas::UI::ok_box();
-	} catch Yaffas::Exception with {
-		print Yaffas::UI::all_error_box(shift);
-		print change_admin_pass();
-	};
-	main::footer();
-} else {
-	# parent
-	wait;
-}
-
-
+	print Yaffas::UI::ok_box();
+} catch Yaffas::Exception with {
+	print Yaffas::UI::all_error_box(shift);
+	print change_admin_pass();
+};
+main::footer();
 
 =pod
 

base/webmin/modules/yaffas-module-changepw/changepw/check_rootpass.cgi
@@ -21,23 +21,15 @@ ReadParse();
 my $pass1 = $main::in{'pass1'};
 my $pass2 = $main::in{'pass2'};
 
-# fork, because we have to restart webmin
-my $pid = fork;
-if ($pid == 0) {
-	# child
-	try {
-		Yaffas::Module::ChangePW::check_passwords($pass1, $pass2);
-		Yaffas::Module::ChangePW::change_root_password($pass1);
+try {
+	Yaffas::Module::ChangePW::check_passwords($pass1, $pass2);
+	Yaffas::Module::ChangePW::change_root_password($pass1);
 
-		print Yaffas::UI::ok_box();
-	} catch Yaffas::Exception with {
-		print Yaffas::UI::all_error_box(shift);
-		print change_root_pass();
-	};
-} else {
-	# parent
-	wait;
-}
+	print Yaffas::UI::ok_box();
+} catch Yaffas::Exception with {
+	print Yaffas::UI::all_error_box(shift);
+	print change_root_pass();
+};
 
 main::footer();
 

commit 4b41da3eb147abf7e958cce719619e38888f7278

Fixing ADM-359 (Accounts > Auth server: AD config errors out)
Got broken in 617b543ff24e82ecf159d3cfb609458182bc02b8

base/webmin/modules/yaffas-module-authsrv/authsrv/check_ads.cgi
@@ -83,14 +83,15 @@ try {
 			control(YAFFAS_MAILDISCLAIMERS(), RESTART());
 		}
 
-	try {
-		Yaffas::Service::control(WEBMIN, RESTART);
-	} catch Yaffas::Exception with {
-		print Yaffas::UI::all_error_box(shift);
-	};
+		try {
+			Yaffas::Service::control(WEBMIN, RESTART);
+		} catch Yaffas::Exception with {
+			print Yaffas::UI::all_error_box(shift);
+		};
 
-	sleep(3);
-	print Yaffas::UI::ok_box();
+		sleep(3);
+		print Yaffas::UI::ok_box();
+	}
 }
 catch Yaffas::Exception with {
 	Yaffas::Service::control(NSCD, START) unless Yaffas::Constant::OS =~ m/RHEL\d/ ;