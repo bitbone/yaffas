98c86c1dda40ab764cc1adf12c48dea6bc3be010
Christian Hoffmann <hoffmann@bitbone.de>
2013-10-08 15:59:42 +0200

    zarafa-wa-plugins: webapp: enable passwd plugin by default again

---
+++ zarafa/zarafa-wa-plugins/webapp/passwd/php/plugin.passwd.php
@@ -0,0 +1,54 @@
+<?php
+  /**
+   * Passwd Plugin
+   * Integrates (AD) password changing functionality
+   * based on http://doc.zarafa.com/trunk/WebApp_Developers_Manual/en-US/html-single/#settings_model
+   */
+  class Pluginpasswd extends Plugin {
+
+    /**
+     * Function initializes the Plugin and registers all hooks
+     * @return void
+     */
+    function init() {
+      $this->registerHook('server.core.settings.init.before');
+    }
+
+    /**
+     * Function is executed when a hook is triggered by the PluginManager
+     * @param string $eventID the id of the triggered hook
+     * @param mixed $data object(s) related to the hook
+     * @return void
+     */
+    function execute($eventID, &$data) {
+      switch($eventID) {
+        case 'server.core.settings.init.before' :
+          $this->injectPluginSettings($data);
+          break;
+      }
+    }
+
+    /**
+     * Called when the core Settings class is initialized and ready to accept
+     * the sysadmin's default settings. Registers the sysadmin defaults
+     * for the plugin.
+     * @param Array $data Reference to the data of the triggered hook
+     */
+    function injectPluginSettings(&$data) {
+      $data['settingsObj']->addSysAdminDefaults(Array(
+        'zarafa' => Array(
+          'v1' => Array(
+            'plugins' => Array(
+              'passwd' => Array(
+                'enable' => PLUGIN_PASSWD_USER_DEFAULT_ENABLE,
+              )
+            )
+          )
+        )
+      ));
+    }
+  }
+  /*
+     vim:ts=2:sw=2:et:
+  */
+?>
