bebf452b980f2594b9d50b954909b44c785160c9
Christian Hoffmann <hoffmann@bitbone.de>
2013-10-16 09:51:43 +0200

    yaffas-zarafa: webapp-1.4 SELinux labelling for /var/lib/zarafa-webapp

--- zarafa/conf/yaffas-zarafa/share/yaffas-upgrade/04-zarafa-webapp-selinux.sh
+++ zarafa/conf/yaffas-zarafa/share/yaffas-upgrade/04-zarafa-webapp-selinux.sh
@@ -7,3 +7,7 @@ getsebool httpd_can_network_connect | grep -q on$ && exit 0
 # WebApp 1.3 and later default to connecting to http://localhost:236
 # instead of using the unix socket /var/run/zarafa (ADM-321)
 setsebool -P httpd_can_network_connect=1
+
+# in WebApp 1.4 the proper SELinux context does not seem to be set
+# by the packages anymore.
+chcon -R -t var_lib_t /var/lib/zarafa-webapp/
