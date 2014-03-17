bitbone/yaffas

commit 84a2c797c684a1531c1d328993ca241c327aa109

UGM: fixing user creation on wheezy

base/perlmod/yaffas-lib/lib/Yaffas/UGM.pm
@@ -285,9 +285,9 @@ sub add_user($$$$@) {
 	my @cmd;
 	push @cmd, Yaffas::Constant::APPLICATION->{useradd}, "-g", $pgid;
 
-	if (Yaffas::Constant::OS eq "Ubuntu" && Yaffas::Constant::OSVER eq "12.04") {
+	if (!((Yaffas::Constant::OS eq "Ubuntu" && Yaffas::Constant::OSVER eq "10.04") || (Yaffas::Constant::OS eq "Debian" && Yaffas::Constant::OSVER eq "6"))) {
 		# we require --non-unique to work around yet another smbldap-useradd
-		# bug, which is sadly in Ubuntu's 12.04 packages;
+		# bug, which is sadly in Ubuntu's 12.04 and Debian 7 packages;
 		# by doing this we circumvent the duplicate uid check, which is
 		# tolerable, as we do our own check...
 		# also ADM-185

commit db79bb09ea90464315e7bcc4f86998e928ebe961

Yaffas::UGM: fixing user creation on RHEL
Got broken in 84a2c797c684a1531c1d328993ca241c327aa109

base/perlmod/yaffas-lib/lib/Yaffas/UGM.pm
@@ -285,7 +285,8 @@ sub add_user($$$$@) {
 	my @cmd;
 	push @cmd, Yaffas::Constant::APPLICATION->{useradd}, "-g", $pgid;
 
-	if (!((Yaffas::Constant::OS eq "Ubuntu" && Yaffas::Constant::OSVER eq "10.04") || (Yaffas::Constant::OS eq "Debian" && Yaffas::Constant::OSVER eq "6"))) {
+	# are we on Ubuntu 12.04+ or Debian 7+?
+	if ((Yaffas::Constant::OS eq "Ubuntu" && Yaffas::Constant::OSVER ne "10.04") || (Yaffas::Constant::OS eq "Debian" && Yaffas::Constant::OSVER ne "6")) {
 		# we require --non-unique to work around yet another smbldap-useradd
 		# bug, which is sadly in Ubuntu's 12.04 and Debian 7 packages;
 		# by doing this we circumvent the duplicate uid check, which is