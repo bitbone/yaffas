bitbone/yaffas

commit 363905d8c056e66b627c93e66ea3df0ee537bd8d

Fixing ADM-399 (Set a default domain on CentOS/RHEL if empty upon install)

base/webmin/modules/yaffas-module-setup/setup/initialsetup.cgi
@@ -37,6 +37,13 @@ try {
 
 	Yaffas::Module::ChangePW::change_admin_password($pw1);
 
+	use Yaffas::Module::Netconf;
+	my $netconf = Yaffas::Module::Netconf->new();
+	if (!defined $n->domainname() || $n->domainname() eq "")
+		$n->domainname("yaffas.local");
+		$n->save();
+	}
+
 	if (Yaffas::Product::check_product("zarafa")) {
 		eval "use Yaffas::Module::ZarafaConf;";
 		Yaffas::Module::ZarafaConf::set_zarafa_database($main::in{mysql_host}, $main::in{mysql_database}, $main::in{mysql_user}, $main::in{mysql_password});

commit e42e37e4558ecf9486918cda7a0ef71b44fd1685

netconf: Fixing perl syntax errors during setup
(introduced in 363905d8c056e66b627c93e66ea3df0ee537bd8d with the fix
for ADM-399)

base/webmin/modules/yaffas-module-setup/setup/initialsetup.cgi
@@ -11,6 +11,7 @@ use Yaffas::Module::ChangePW;
 use Yaffas::UGM;
 use Yaffas::Service qw(NSCD GOGGLETYKE WINBIND WEBMIN STOP START RESTART SAMBA ZARAFA_SERVER POSTFIX USERMIN);
 use Yaffas::Module::Mailsrv::Postfix qw(set_accept_domains set_smarthost);
+use Yaffas::Module::Netconf;
 
 Yaffas::init_webmin();
 
@@ -37,11 +38,10 @@ try {
 
 	Yaffas::Module::ChangePW::change_admin_password($pw1);
 
-	use Yaffas::Module::Netconf;
 	my $netconf = Yaffas::Module::Netconf->new();
-	if (!defined $n->domainname() || $n->domainname() eq "")
-		$n->domainname("yaffas.local");
-		$n->save();
+	if (!defined $netconf->domainname() || $netconf->domainname() eq "") {
+		$netconf->domainname("yaffas.local");
+		$netconf->save();
 	}
 
 	if (Yaffas::Product::check_product("zarafa")) {