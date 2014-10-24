bitbone/yaffas

commit 59ed2a2ff49b3159c420cbc2767cfea78ba8e659

ADM-415: Hostname config fails on Ubuntu 14.04

base/webmin/modules/yaffas-module-netconf/lib/Netconf.pm
@@ -658,7 +658,11 @@ sub _save_hostname {
 	# call /etc/init.d/hostname.sh
 	if(Yaffas::Constant::get_os() eq "Ubuntu" or Yaffas::Constant::OS eq "Debian"){
 		my $app = Yaffas::Constant::APPLICATION->{"hostname.sh"};
-		system($app, "start");
+		if (ref($app) == "ARRAY") {
+			system(@{$app}, "start");
+		} else {
+			system($app, "start");
+		}
 		throw Yaffas::Exception("err_file_execute", $app) unless($?>>8 == 0);
 	}
 	else {

base/perlmod/yaffas-lib/lib/Yaffas/Constant.pm
@@ -331,7 +331,7 @@ use constant {
 		"faxcron" => "/usr/sbin/faxcron",
 		"faxstat" => "/usr/bin/faxstat",
 		"openssl" => "/usr/bin/openssl",
-		"hostname.sh" => (OS eq 'Debian' ? "/etc/init.d/hostname.sh" : "/etc/init.d/hostname"),
+		"hostname.sh" => ((OS eq 'Ubuntu' and OSVER eq '14.04') ? ["service", "hostname"] : (OS eq 'Debian' ? "/etc/init.d/hostname.sh" : "/etc/init.d/hostname")),
 		"dnsdomainname" => ( Yaffas::Constant::OS eq 'Ubuntu' or OS eq 'Debian' ) ? "/bin/dnsdomainname" : "/bin/domainname",
 		"nscd" => "/usr/sbin/nscd",
 		"sievec" => "/usr/lib/cyrus/bin/sievec",

commit 9f2a2027e2bcd479ff2bf2a38d8b41fa35021eeb

Fixing hostname config changes after breakage in 59ed2a2ff49b3159c420cbc2767cfea78ba8e659

base/webmin/modules/yaffas-module-netconf/lib/Netconf.pm
@@ -658,7 +658,7 @@ sub _save_hostname {
 	# call /etc/init.d/hostname.sh
 	if(Yaffas::Constant::get_os() eq "Ubuntu" or Yaffas::Constant::OS eq "Debian"){
 		my $app = Yaffas::Constant::APPLICATION->{"hostname.sh"};
-		if (ref($app) == "ARRAY") {
+		if (ref($app) eq "ARRAY") {
 			system(@{$app}, "start");
 		} else {
 			system($app, "start");