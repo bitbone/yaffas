360b8cb77468318c5fd38d6dd6bd37b57ac8fd27
Christian Hoffmann <hoffmann@bitbone.de>
2015-11-18 15:31:44 +0100

    yaffas-zarafa: fix php-mapi.log permissions

--- zarafa/conf/yaffas-zarafa/share/yaffas-upgrade/06-zarafa-mapi-log-perms.sh
+++ zarafa/conf/yaffas-zarafa/share/yaffas-upgrade/06-zarafa-mapi-log-perms.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 MAPI_LOG=/var/log/zarafa/php-mapi.log
-chown :www-data "$MAPI_LOG"
+touch "$MAPI_LOG"
+
+OWNER=www-data
+getent group apache >/dev/null 2>&1 && OWNER=apache
+chown :"$OWNER" "$MAPI_LOG"
 chmod 660 "$MAPI_LOG"
